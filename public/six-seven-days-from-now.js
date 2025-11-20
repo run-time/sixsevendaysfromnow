f68b32f58aa9099552ef0c9ffb171a3cb5c47e1e
Dave Alger <run-time@users.noreply.github.com>
2025-11-20 13:47:42 -0700

    Change font size from 16vw to 24vmin

--- public/six-seven-days-from-now.js
+++ public/six-seven-days-from-now.js
@@ -50,7 +50,7 @@ class SixSevenDaysFromNow extends HTMLElement {
                 }
 
                 .days-number {
-                    font-size: 16vw;
+                    font-size: 24vmin;
                     font-weight: 900;
                     background: linear-gradient(135deg, #368dda 0%, #764ba2 100%);
                     -webkit-background-clip: text;
